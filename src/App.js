6fc2ffcea0357c59cc9a540daeec225ccb39a34b
Aluri varaprasad <38999815+varaprasadaluri54@users.noreply.github.com>
2022-03-20 01:25:19 +0530

    img changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import NavBar from "./components/Navbar";
 import "./styles.css";
-import "../images/12.png";
+
 export default function App() {
   return (
     <div className="App">
       <NavBar />
       <section id="hero" className="d-flex align-items-center">
         <div className="container text-center position-relative">
-          <img src="../images/12.png" alt="hi" />
+          <img src="./images/12.jpg" alt="hi" />
           <h1>24/7 care is Availble</h1>
           <h2>Welcome everyone</h2>
           <a href="#" className="main-btn">
